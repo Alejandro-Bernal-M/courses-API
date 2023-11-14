03a5aeba4f77110b58a725cdcff2d1644a6bbd55
alejandro <alejandrober96@gmail.com>
2023-11-14 14:21:44 -0500

    Add course model

---
+++ src/models/course.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+
+const CourseSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  instructor: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    max: 200,
+    min: 10,
+    trim: true,
+    required: true
+  },
+  enrollmentStatus:{
+    type: String,
+    enum: ['Open', 'Closed', 'InProgress'],
+    required: true
+  },
+  thumbnail: {
+    type: String,
+    required: true
+  },
+  schedule: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  prerequisites: [
+    {
+      type: String
+    }
+  ],
+  syllabus: [
+    {
+      week: Number,
+      topic: String,
+      content: String
+    }
+  ],
+  students: [
+    {
+      type: mongoose.Types.ObjectId, ref: 'User'
+    }
+  ]
+});
+
+module.exports = mongoose.model('Course', CourseSchema);
