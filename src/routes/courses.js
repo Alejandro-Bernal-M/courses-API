5ed20604137420deeb3c4e5ac8d263a8e4bcf0c7
alejandro <alejandrober96@gmail.com>
2023-11-14 15:45:19 -0500

    Add post route for creating courses

---
+++ src/routes/courses.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const { requireSignin, adminMiddleware, userMiddleware } = require('../common-middlewares');
+const { createCourse } = require('../controller/course');
+const multer = require('multer');
+const shortid = require('shortid');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(path.dirname(__dirname), 'uploads'))
+  },
+  filename: function (req, file, cb) {
+    cb(null, shortid.generate() + '-' + file.originalname)
+  }
+})
+
+const upload = multer({storage: storage});
+
+router.post('/admin/courses/create', requireSignin, adminMiddleware,upload.single('thumbnail'), createCourse);
+
+module.exports = router;
