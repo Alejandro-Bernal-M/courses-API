3e91e7a7a842bf720a89541c5fa835035977a746
alejandro <alejandrober96@gmail.com>
2023-11-14 15:45:49 -0500

    Add public path to display the courses thumbnail

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const path = require('path');
 //routes files
 const authRoutes = require('./routes/auth');
+const courseRoutes = require('./routes/courses');
 
 // env
 env.config();
@@ -14,9 +15,10 @@ mongoose.connect(process.env.MONGO).then(() => console.log('Database connected')
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
+app.use('/public',express.static(path.join(__dirname, 'uploads')));
 //routes middlewares
 app.use('/api' , authRoutes);
+app.use('/api/', courseRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port = ${process.env.PORT}`)
