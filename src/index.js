7de9ec7fa553629f1fc8c991e30a90eba13a5a5d
Alejandro Bernal Marin <alejandrober96@gmail.com>
2023-11-15 16:45:14 -0500

    Add cors config

--- src/index.js
+++ src/index.js
@@ -9,7 +9,32 @@ const courseRoutes = require('./routes/courses');
 const userRoutes = require('./routes/user');
 // Redoc
 const fs = require('fs');
+//cors
+const cors = require('cors');
 
+// env
+env.config();
+
+//connect db
+mongoose.connect(process.env.MONGO).then(() => console.log('Database connected'));
+
+//cors options
+app.options('*', cors());
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/public',express.static(path.join(__dirname, 'uploads')));
+app.use(cors({
+  origin: process.env.ORIGIN, // or an array of allowed origins
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,  // Enable credentials (cookies, authorization headers, etc.)
+}));
+
+//routes middlewares
+app.use('/api' , authRoutes);
+app.use('/api/', courseRoutes);
+app.use('/api/', userRoutes);
 
 app.use('/docs', (req, res) => {
   fs.readFile('redoc-static.html', 'utf8', (err, data) => {
@@ -22,21 +47,6 @@ app.use('/docs', (req, res) => {
     res.send(data);
   });
 } );
-
-// env
-env.config();
-
-//connect db
-mongoose.connect(process.env.MONGO).then(() => console.log('Database connected'));
-
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use('/public',express.static(path.join(__dirname, 'uploads')));
-//routes middlewares
-app.use('/api' , authRoutes);
-app.use('/api/', courseRoutes);
-app.use('/api/', userRoutes);
-
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port = ${process.env.PORT}`)
 });
